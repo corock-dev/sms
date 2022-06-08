2b5acd6e549505fc72cf9169e557ada6af574260
cororing <corock.dev@gmail.com>
2022-06-08 09:11:48 +0900

    1일차까지 일단 커밋

--- src/main/java/com/nhnacademy/sms/SmsApplication.java
+++ src/main/java/com/nhnacademy/sms/SmsApplication.java
@@ -1,13 +1,25 @@
 package com.nhnacademy.sms;
 
+import com.nhnacademy.sms.domain.Student;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SmsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SmsApplication.class, args);
+        // SpringApplication.run(SmsApplication.class, args);
+
+        SpringApplication springApplication = new SpringApplication(SmsApplication.class);
+        springApplication.run(args);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+
+        // new SpringApplicationBuilder().sources(Student.class).web(WebApplicationType.NONE)
+        //         .child(FirstChildConfig.class).web(WebApplicationType.SERVLET)
+        //         .sibling()
     }
 
 }
